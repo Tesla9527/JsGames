d115ae9f96a972f3ed40dbd0f318fb0ceaa29e54
hakubox <lidian_226@163.com>
2016-09-18 23:33:20 +0800

    123

---
+++ 贪吃蛇/js/canvas.js
@@ -0,0 +1,31 @@
+﻿
+/**
+ * 画布
+ * @class Canvas
+ * @constructor 构造函数
+ * @param {String} element_id 画布所属元素Id.
+ */
+function Canvas(element_id) {
+    /**
+     * 宽度（横向格数）
+     * @property {Number} Width
+     * @private
+     */
+    this.Width = 0;
+    /**
+     * 高度（纵向格数）
+     * @property {Number} Height
+     * @private
+     */
+    this.Height = 0;
+    /**
+     * 物品宽度
+     * @property {Number} GoodsWidth
+     */
+    this.GoodsWidth = 0;
+    /**
+     * 物品宽度
+     * @property {Number} GoodsWidth
+     */
+    this.GoodsHeight = 0;
+}
