d0b085d2e491bc5a266c682603c04da683025728
Tesla Lau <tesla9527@gmail.com>
2016-09-18 23:31:37 +0800

    111

---
+++ 贪吃蛇/js/Enum.js
@@ -0,0 +1,22 @@
+﻿/**
+ * 物品类别
+ * @enum {Number} Enum.GoodsType
+ */
+var GoodsType = {
+    /**
+    * 未指定
+    */
+    Null: 0,
+    /**
+     * 玩家
+     */
+    Player: 1, 
+    /**
+     * 路
+     */
+    Road: 2,  
+    /**
+     * 障碍物
+     */
+    Bar: 3
+}
